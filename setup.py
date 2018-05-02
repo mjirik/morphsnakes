b5e33450935f9401c005c9ff2701266abd32d399
Miroslav Jirik <miroslav.jirik@gmail.com>
2018-05-02 16:07:46 +0200

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 # from distutils.core import setup
 
 setup(name='morphsnakes',
-      version='0.0.10',
+      version='0.0.11',
       requires=['scipy'],
       py_modules=['morphsnakes'])
